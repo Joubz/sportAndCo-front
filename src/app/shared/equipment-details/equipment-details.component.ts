51608d0f6b5d36a4b0a91c6b5b64624bc8b82c55
Joubz <nathan.joubert.pro33@gmail.com>
2021-05-05 11:52:56 +0200

    feat: initialisation branche

---
+++ src/app/shared/equipment-details/equipment-details.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-equipment-details',
+  templateUrl: './equipment-details.component.html',
+  styleUrls: ['./equipment-details.component.css']
+})
+export class EquipmentDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { EquipmentDetailsComponent } from './equipment-details/equipment-details.component';
 
 /**
  * Gestion des modules et composants de Shared
@@ -9,6 +10,8 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   declarations: [
 
+  
+    EquipmentDetailsComponent
   ],
   imports: [
     CommonModule,
