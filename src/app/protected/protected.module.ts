8b9a28c5d8591387c17a77217dc6c91cf9df97fb
Joubz <nathan.joubert.pro33@gmail.com>
2021-05-07 12:07:10 +0200

    refacto: suppression du composant exemple de protected

--- src/app/protected/protected.module.ts
+++ src/app/protected/protected.module.ts
@@ -2,7 +2,6 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { ProtectedRoutingModule } from './protected-routing.module';
-import { ExempleComponentComponent } from './exemple-component/exemple-component.component';
 import { EquipmentDetailsComponent } from './equipment-details/equipment-details.component';
 import { ListEquipmentComponent } from './list-equipment/list-equipment.component';
 import {DatepickerModule} from "ng2-datepicker";
@@ -12,7 +11,7 @@ import {DatepickerModule} from "ng2-datepicker";
  * Gestion des modules et composants de Protected
  */
 @NgModule({
-  declarations: [ExempleComponentComponent, EquipmentDetailsComponent, ListEquipmentComponent],
+  declarations: [EquipmentDetailsComponent, ListEquipmentComponent],
   imports: [
     CommonModule,
     ProtectedRoutingModule,
